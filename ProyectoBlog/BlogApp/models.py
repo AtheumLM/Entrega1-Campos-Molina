a1bf7b56c00f0f1080f3dc95907ee36e168ad333
lemolina <lautaro677@gmail.com>
2022-06-24 11:36:03 -0300

    Clase Categoria Creada y BD respectiva

--- ProyectoBlog/BlogApp/models.py
+++ ProyectoBlog/BlogApp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Categoria(models.Model):
+
+  Nombre = models.CharField(max_length=50)
+  descripcion = models.CharField(max_length=200)
+  
+
+
